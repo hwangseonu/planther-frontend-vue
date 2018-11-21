1d4146fac2b32a9786d5f87bd560083e8a0d2fbb
hwangseonu <hwangseonu12@naver.com>
2018-11-21 20:12:19 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <page-header></page-header>
+  </div>
+</template>
+
+<script>
+import PageHeader from '@/components/PageHeader'
+
+export default {
+  name: 'App',
+  components: {PageHeader}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "assets/styles/style";
+#app {
+}
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="page-header">
+    <ul>
+      <li v-for="(text, index) in menu" :key="index">{{ text }}</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader',
+  data () {
+    return {
+      menu: [
+        'Home',
+        'Posts'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
