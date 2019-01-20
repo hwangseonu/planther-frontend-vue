c5127432537b818a02fa1fcf08f7ebc5785e6dd5
hwangseonu <hwangseonu12@naver.com>
2019-01-20 19:47:29 +0900

    feat: Add main page

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,5 +1,16 @@
 <template>
-  <navbar></navbar>
+  <div id="main">
+    <navbar></navbar>
+    <div class="page-header">
+      <div class="title-wrapper">
+        <p class="page-title">Planther</p>
+        <p class="page-description">소중한 우리들의 일정을 위한 서비스</p>
+        <button>Go to calendar</button>
+      </div>
+    </div>
+    <div class="page-section">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,5 +23,49 @@
 </script>
 
 <style>
+  #main {
+    width: 100%;
+    min-height: 100vh;
+    background: url("../assets/images/background.png") no-repeat center center fixed;
+    background-size: cover;
+  }
 
+  .page-header {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 70vh;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  .title-wrapper {
+    margin-top: 70px;
+    margin-left: 12.6vw;
+    color: #FFF;
+  }
+
+  .page-title {
+    font-size: 4em;
+    font-weight: bold;
+  }
+
+  .page-description {
+    font-weight: lighter;
+  }
+
+  .title-wrapper button {
+    margin-top: 1em;
+    width: 200px;
+    height: 50px;
+    background-color: #f2c61d;
+    border-radius: 30px;
+    font-size: 1.3em;
+    color: #FFF;
+  }
+
+  .page-section {
+    width: 100%;
+    height: 70vh;
+    background-color: #FFF;
+  }
 </style>
